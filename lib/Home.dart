8bca2dc05d98085792061e109422950ec3cf052f
abdelghafourbk <abdelghafoubk@gmail.com>
2022-06-30 21:13:00 +0100

    updated pieces logic

--- lib/Home.dart
+++ lib/Home.dart
@@ -26,6 +26,32 @@ class _HomePageState extends State<HomePage> {
   var blackDeadPieces = [];
   bool whiteTurn = true; //=> white goes first
 
+  // pieces
+  var pieces = [
+    //the strcuture going to be like this:
+    // [position, color, selected, possibleKills]
+
+    //Black Pieces
+    [0, 'rook', 'black', 'unselected', ''],
+    [2, 'rook', 'black', 'unselected', ''],
+    [3, 'rook', 'black', 'unselected', ''],
+    [4, 'rook', 'black', 'unselected', ''],
+    [5, 'rook', 'black', 'unselected', ''],
+    [6, 'rook', 'black', 'unselected', ''],
+    [7, 'rook', 'black', 'unselected', ''],
+    [8, 'rook', 'black', 'unselected', ''],
+    [9, 'rook', 'black', 'unselected', ''],
+    [10, 'rook', 'black', 'unselected', ''],
+    [11, 'rook', 'black', 'unselected', ''],
+    [12, 'rook', 'black', 'unselected', ''],
+    [13, 'rook', 'black', 'unselected', ''],
+    [14, 'rook', 'black', 'unselected', ''],
+    [15, 'rook', 'black', 'unselected', ''],
+
+    // open spaces => w= blank square, 0 = possible move, color, k = possible kill
+    [16, 'rook', 'black', 'unselected', ''],
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold();
